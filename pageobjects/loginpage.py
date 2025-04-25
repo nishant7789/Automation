060b498bf056b3121c11ad77c1d1b33275cb15e3
Nishant Ranjan <101732677+nishant7789@users.noreply.github.com>
2025-04-25 14:40:54 +0530

    Add files via upload

--- pageobjects/loginpage.py
+++ pageobjects/loginpage.py
@@ -16,6 +16,8 @@ class LoginPage:
         self.username = (By.NAME, 'username')
         self.password = (By.NAME, "password")
         self.login_page = (By.XPATH, "//h5[text()='Login']")
+        self.submit_btn = (By.XPATH, "//button[@type='submit']")
+        self.invalid_credentials_error_message = (By.CSS_SELECTOR, ".oxd-alert--error")
 
     def get_username_field(self):
         return self.driver.find_element(*self.username)
@@ -24,3 +26,12 @@ class LoginPage:
         login_text = WebDriverWait(self.driver, config["timeout"]).until(
             EC.presence_of_element_located(self.login_page))
         return login_text
+
+    def get_password_field(self):
+        return self.driver.find_element(*self.password)
+
+    def get_submit_btn(self):
+        return self.driver.find_element(*self.submit_btn)
+
+    def get_error_message_invalid_credentials(self):
+        return self.driver.find_element(*self.invalid_credentials_error_message)
